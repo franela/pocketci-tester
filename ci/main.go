42c3996eddca0ebf02ad05fed546ff7902349ead
Matias Pan <gh@matiaspan.dev>
2024-08-30 14:02:22 -0300

    Branch used in the context of pocketci integration tests (#1)
    * Branch used in the context of pocketci integration tests
    
    Signed-off-by: Matias Pan <gh@matiaspan.dev>
    
    * Commit used for the synchronize webhook test in pocketci
    
    Signed-off-by: Matias Pan <gh@matiaspan.dev>
    
    * Update with latest features
    
    Signed-off-by: Matias Pan <gh@matiaspan.dev>
    
    ---------
    
    Signed-off-by: Matias Pan <gh@matiaspan.dev>

--- ci/main.go
+++ ci/main.go
@@ -8,7 +8,23 @@ import (
 
 type Ci struct{}
 
-func (m *Ci) Dispatch(ctx context.Context, src *dagger.Directory, eventTrigger *dagger.File) error {
+// Dispatch function for pocketci
+func (m *Ci) Dispatch(ctx context.Context, vendor, event, filter string, src *dagger.Directory, eventTrigger *dagger.File) error {
 	fmt.Println(eventTrigger.Contents(ctx))
+
+	ci := dag.Pocketci(eventTrigger)
+	if ci.OnPullRequest() != nil {
+		fmt.Println("Received On Pull Request")
+	}
+	if ci.OnCommitPush() != nil {
+		fmt.Println("Receinved OnCommitPush")
+	}
+
 	return nil
+}
+
+func (m *Ci) OnPullRequest(ctx context.Context) {
+}
+
+func (m *Ci) OnCommitPush(ctx context.Context, src *dagger.Directory, eventTrigger *dagger.File) {
 }
