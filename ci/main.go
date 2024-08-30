dfe65b129f357672552d6a28b0c711710a8f3750
Matias Pan <gh@matiaspan.dev>
2024-08-30 13:59:31 -0300

    Update with latest features
    Signed-off-by: Matias Pan <gh@matiaspan.dev>

--- ci/main.go
+++ ci/main.go
@@ -9,7 +9,22 @@ import (
 type Ci struct{}
 
 // Dispatch function for pocketci
-func (m *Ci) Dispatch(ctx context.Context, src *dagger.Directory, eventTrigger *dagger.File) error {
+func (m *Ci) Dispatch(ctx context.Context, vendor, event, filter string, src *dagger.Directory, eventTrigger *dagger.File) error {
 	fmt.Println(eventTrigger.Contents(ctx))
+
+	ci := dag.Pocketci(eventTrigger)
+	if ci.OnPullRequest() != nil {
+		fmt.Println("Received On Pull Request")
+	}
+	if ci.OnCommitPush() != nil {
+		fmt.Println("Receinved OnCommitPush")
+	}
+
 	return nil
+}
+
+func (m *Ci) OnPullRequest(ctx context.Context) {
+}
+
+func (m *Ci) OnCommitPush(ctx context.Context, src *dagger.Directory, eventTrigger *dagger.File) {
 }
